c537bbfc20edbbe428d0c9bca7b75f93ec49dc58
PHAM MINH CHI <DESKTOP-DAOFM1M>
2020-06-06 23:08:10 +0700

    Thêm nút nhấn đăng ký tham gia khóa học

--- BigSchool/Controllers/AttendancesController.cs
+++ BigSchool/Controllers/AttendancesController.cs
@@ -1,4 +1,5 @@
-﻿using BigSchool.Models;
+﻿using BigSchool.DTOs;
+using BigSchool.Models;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -20,17 +21,37 @@ namespace BigSchool.Controllers
             _dbContext = new ApplicationDbContext();
         }
         [HttpPost]
-        public IHttpActionResult Attend([FromBody] int courseId)
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
         {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exists!");
+
             var attendance = new Attendance
             {
-                CourseId = courseId,
-                AttendeeId = User.Identity.GetUserId()
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
             };
             _dbContext.Attendances.Add(attendance);
             _dbContext.SaveChanges();
 
             return Ok();
         }
+        //public IHttpActionResult Attend([FromBody] int courseId)
+        //{
+        //    var userId = User.Identity.GetUserId();
+        //    if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == courseId))
+        //        return BadRequest("The Attendance already exists!");
+
+        //    var attendance = new Attendance
+        //    {
+        //        CourseId = courseId,
+        //        AttendeeId = userId
+        //    };
+        //    _dbContext.Attendances.Add(attendance);
+        //    _dbContext.SaveChanges();
+
+        //    return Ok();
+        //}
     }
 }
