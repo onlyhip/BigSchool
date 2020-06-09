8597f97202c9dae8c01fc46e7cd539f942cf560d
PHAM MINH CHI <DESKTOP-DAOFM1M>
2020-06-09 21:19:27 +0700

    Xem danh sách khóa học đăng ký tham dự

--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -1,7 +1,9 @@
 ﻿using BigSchool.Models;
+using BigSchool.ViewModels;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -50,6 +52,27 @@ namespace BigSchool.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+
+        [Authorize]
+        public ActionResult Attending()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var courses = _dbContext.Attendances
+                .Where(a => a.AttendeeId == userId)
+                .Select(a => a.Course)
+                .Include(l => l.Lecturer)
+                .Include(l => l.Category)
+                .ToList();
+
+            var viewModel = new CoursesViewModel
+            {
+                UpcommingCourses = courses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+
+            return View(viewModel);
+        }
 
     }
 }
